99180e548fa6eeabaa1b83f05e2e07fc3255fe89
Kebron <Kebronjrdaniel@gmail.com>
2020-12-19 14:33:13 +0300

    Loop through the items in the model and display them in the home-page

--- laundry/models.py
+++ laundry/models.py
@@ -1,9 +1,24 @@
 from django.db import models
 from django.conf import settings
 
+CATEGORY_CHIOCES = (
+    ('SU', 'Suit'),
+    ('SH', 'Shirt'),
+    ('TR', 'Trouser'),
+    ('HS', 'HomeSheet'),
+    ('JK', 'Jacket')
+)
+LABEL_CHIOCES = (
+    ('P', 'Primary'),
+    ('S', 'Secondary'),
+    ('D', 'Danger')
+)
+
 class Item(models.Model):
     title = models.CharField(max_length=100)
     price = models.FloatField()
+    category = models.CharField(choices=CATEGORY_CHIOCES, max_length=2)
+    label = models.CharField(choices=LABEL_CHIOCES, max_length=1)
 
     def __str__(self):
         return self.title
