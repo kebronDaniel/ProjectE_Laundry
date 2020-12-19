8f86511d6466fb6a8e6f1f397ebfbe9aaf0e2ceb
Kebron <Kebronjrdaniel@gmail.com>
2020-12-19 11:36:54 +0300

    Created a basic model and also used the django all auth library for authentication

--- ProjectE_Laundry/settings.py
+++ ProjectE_Laundry/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'laundry.apps.LaundryConfig',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+
 ]
 
 MIDDLEWARE = [
@@ -119,3 +125,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+SITE_ID = 1
+
