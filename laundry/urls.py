8cea9e8f6c6e6b950f1d5520efbfdd73396b2ed5
Kebron <Kebronjrdaniel@gmail.com>
2020-12-19 14:06:32 +0300

    Made a base template and changed the nav bar accordingly

--- laundry/urls.py
+++ laundry/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path,include
-from .views import item_list
+from .views import item_list,item,checkout
 
 
 urlpatterns = [
     path('',item_list, name = 'item-list'),
+    path('product/',item, name = 'item'),
+    path('checkout/', checkout, name = 'checkout')
 ]
--- laundry/views.py
+++ laundry/views.py
@@ -7,3 +7,15 @@ def item_list(request):
         "items": Item.objects.all()
     }
     return render(request, 'home-page.html', context)
+
+def item(request):
+    context = {
+        "items": Item.objects.all()
+    }
+    return render(request,'product-page.html', context)
+
+def checkout(request):
+    context = {
+        "items": Item.objects.all()
+    }
+    return render(request, 'checkout-page.html', context)
