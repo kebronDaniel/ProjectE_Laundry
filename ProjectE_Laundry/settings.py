1e6b088310363ad5cc3c53c9ba533f0ae7c22bfb
Kebron <Kebronjrdaniel@gmail.com>
2020-12-20 15:57:53 +0300

    Display cart total quantity in the nav bar

--- ProjectE_Laundry/settings.py
+++ ProjectE_Laundry/settings.py
@@ -72,6 +72,11 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+
+            'libraries':{
+                'cart_template_tags': 'template_tags.cart_template_tags',
+
+            }
         },
     },
 ]
